13c06562c40725bf7404412a9e23c642582d0627
Prince2222 <71544554+Prince2222@users.noreply.github.com>
2020-09-27 12:31:50 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(4)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendNumber(5)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(3)
+})
+radio.setGroup(2)
